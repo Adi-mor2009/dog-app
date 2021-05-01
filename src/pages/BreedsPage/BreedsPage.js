c51f4933d48fcf27daedfcbd84a0bf847ee8ff1a
Adi-mor2009 <adijmy@gmail.com>
2021-05-01 13:16:30 +0300

    Filter component - dropdowm with filter

--- src/pages/BreedsPage/BreedsPage.js
+++ src/pages/BreedsPage/BreedsPage.js
@@ -3,12 +3,16 @@ import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import BreedCard from '../../components/BreedCard/BreedCard';
 import BreedModel from '../../model/BreedModel';
+import Filter from '../../components/Filter/Filter';
 import './BreedsPage.css';
 
 
 function BreedsPage() {
-    const [breeds, setBreeds] = useState([]);
-    const BreedCards = breeds !== undefined ? breeds.map((breed) => <BreedCard breed={breed}></BreedCard>) : [];
+    const [breeds, setBreeds] = useState([new BreedModel(-1, "All", "")]);
+    const [breedSelected, setBreedSelected] = useState(-1);
+    const BreedCards = breedSelected != -1 ? 
+                    (breeds.filter((breed) => breedSelected == breed.id).map((breed) => <BreedCard key={(breed.id).toString()} breed={breed}></BreedCard>)) :
+                    breeds.map((breed) => breed.id != -1 ? <BreedCard key={(breed.id).toString()} breed={breed}></BreedCard> : <></>);
 
 
     useEffect(() => {
@@ -18,7 +22,6 @@ function BreedsPage() {
             allBreeds.forEach ((breed, index) => {
                 const getBreedURL = "https://dog.ceo/api/breed/" + breed + "/images/random";
                 axios.get(getBreedURL).then(response2 => {
-                    console.log(response2.data);
                     let img = response2.data.message;
                     setBreeds(breeds => [...breeds, new BreedModel(index, breed, img)]);
                 });
@@ -26,9 +29,15 @@ function BreedsPage() {
         });
     }, []);
 
+    function handleBreedSelect(breedId) {
+        console.log(breedId);
+        setBreedSelected(breedId);
+    }
+
     return (
         <div className="p-breeds">
             <Container>
+                <Filter breeds={breeds} activeBreed={breeds[0]} handleBreedSelect={handleBreedSelect}></Filter>
                 Dog Book
                 <div id="main_cards">
                     {BreedCards}
